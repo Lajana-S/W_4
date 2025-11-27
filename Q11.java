2abd15fdffb2ec4e1f1af370cf520caa7a7bb263
Lajana <shakyalajana1637@gmail.com>
2025-11-28 00:37:07 +0545

    Initial sharing of project

---
+++ Q11.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+/**
+ * Workshop_4 Question_11
+ *
+ * @author Lajana
+ * @version v1.0
+ */
+public class Q11
+{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the Annual Income in NRP:");
+        double ai = scan.nextFloat();
+        double tax;
+        double t1,t2,t3,t4,t5,t6;
+        scan.nextLine();
+        if (ai > 0 && ai <= 500000)
+        {
+            t1 = ai/100;
+            t2=0; t3=0; t4=0; t5=0; t6=0;
+        }
+        else if (ai > 500000 && ai <= 700000)
+        {
+            t1 = 500000/100;
+            t2 =(ai-500000)*10/100;
+            t3 =0; t4=0; t5=0; t6=0;
+        }
+        else if (ai > 700000 && ai <= 1000000)
+        {
+            t1 = 500000/100;
+            t2 =200000*10/100;
+            t3 =(ai-700000)*20/100; t4=0; t5=0; t6=0;
+        }
+        else if (ai > 1000000 && ai <= 2000000)
+        {
+            t1 = 500000/100;
+            t2 = 200000*10/100;
+            t3 = 300000*20/100;
+            t4 = (ai-1000000)*30/100; t5=0; t6=0;
+        }
+        else if(ai > 2000000 && ai <= 5000000)
+        {
+            t1 = 500000/100;
+            t2 =200000*10/100;
+            t3 =300000*20/100;
+            t4 =1000000*30/100;
+            t5 =(ai-2000000)*36/100; t6=0;
+        }
+        else
+        {
+            t1 = 500000/100;
+            t2=200000*10/100;
+            t3=300000*20/100;
+            t4=1000000*30/100;
+            t5=2000000*36/100;
+            t6=(ai-5000000)*39/100;
+        }
+        tax = t1 + t2 + t3 + t4 + t5 + t6;
+        System.out.println("Total Tax Payable for Rs."+ai+" is "+tax+".");
+    }
+}
---
+++ Scenario_4.java
@@ -0,0 +1,13 @@
+
+/**
+ * Workshop_4 Scenario
+ *
+ * @author Lajana
+ * @version v1.0
+ */
+public class Scenario_4
+{
+    public static void main(String[] args){
+        System.out.println("Hi");
+    }
+}
