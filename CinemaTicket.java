26341f3a701002daa06100985095ac03c99869e1
Lajana <shakyalajana1637@gmail.com>
2025-11-28 21:25:44 +0545

    Workshop_4

--- CinemaTicket.java
+++ CinemaTicket.java
@@ -12,13 +12,7 @@ public class CinemaTicket
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter your age:");
         int age = scan.nextInt();
-        scan.nextLine();
-        System.out.println("Enter movie language:");
-        String lang = scan.next();
-        System.out.println("Are you a student? (Y/N)");
-        char s = scan.next().charAt(0);
-        System.out.println("Is today a festival day? (Y/N)");
-        char f = scan.next().charAt(0);
+        scan.nextLine();       
         
         int bp;
         
@@ -37,47 +31,70 @@ public class CinemaTicket
         else
         {
             bp=0;
-            System.out.println("Invalid age");
+            System.out.println("Invalid age!!");
         }
+        
+        System.out.println("Enter movie language: (Nepali/English/Hindi)");
+        String lang = scan.next();
         
         int lp;
         
-        if (lang == "Hindi")
+        if (lang.equals("Nepali") || lang.equals("nepali"))
+        {
+            lp=bp;
+        }
+        else if (lang.equals("Hindi") || lang.equals("hindi"))
         {
             lp=bp+50;
         }
-        else if (lang == "English")
+        else if (lang.equals("English") || lang.equals("english"))
         {
             lp=bp+100;
         }
         else
         {
             lp=0;
-            System.out.println("Invalid language");
+            System.out.println("Invalid language!!");
         }
+        
+        System.out.println("Are you a student? (Y/N)");
+        char s = scan.next().charAt(0);
         
         float p;
         
-        if (s == 'Y')
+        if (s == 'Y' || s == 'y')
         {
             System.out.println("Is a student");
             p=lp-(lp*20/100);
         }
-        else
+        else if (s == 'N' || s == 'n')
         {
             System.out.println("Is not a student");
             p=lp;
         }
+        else
+        {
+            System.out.println("Invalid student input!!");
+            p=lp;
+        }
+        
+        System.out.println("Is today a festival day? (Y/N)");
+        char f = scan.next().charAt(0);
         
         float tp;
-        if (f == 'Y')
+        if (f == 'Y' || f == 'y')
         {
             System.out.println("It is a festival day");
             tp=p-(p*15/100);
         }
-        else
+        else if (f == 'N' || f == 'n')
         {
             System.out.println("It is not a festival day");
+            tp=p;
+        }
+        else
+        {
+            System.out.println("Invalid festival input!!");
             tp=p;
         }
         System.out.println("");
